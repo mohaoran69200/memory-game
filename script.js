051c134c2dff8417051d80acf1fc64e6d4edd74b
Mohamed Gounani <mo.gounani@gmail.com>
2024-03-22 16:54:37 +0100

    v2.0

--- script.js
+++ script.js
@@ -11,20 +11,48 @@ const images = [
 const cards = [...images, ...images];
 cards.sort(() => Math.random() - 0.5);
 
+let previousCard = null; // Variable pour stocker la carte précédemment cliquée
+
 function createCard(image) {
     return `
-    <div class="card">
         <div class="card-off"><img src="assets/default.png" alt="Card Back"></div>
-        <div class="card-on"><img src="${image}" alt="Card Front"></div>
-    </div>
+        <div class="card-on hidden"><img src="${image.src}" alt=""></div>   
     `;
 }
 
 const gridContainer = document.getElementById('memory-grid');
 
 cards.forEach(image => {
-    const cardElement = document.createElement('div'); // Créer un élément de carte
-    cardElement.classList.add('card'); // Ajouter la classe 'card' à l'élément de carte
-    cardElement.innerHTML = createCard(image); // Ajouter le contenu de la carte avec l'image
-    gridContainer.appendChild(cardElement); // Ajouter l'élément de carte au conteneur de cartes
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card');
+    cardElement.innerHTML = createCard(image);
+    gridContainer.appendChild(cardElement);
 });
+
+const cardsElements = document.querySelectorAll('.card');
+let clicks = 0;
+
+function flipCard() {
+    clicks++;
+    document.getElementById('count').textContent = `Compteur : ${clicks}`;
+
+    const cardOn = this.querySelector('.card-on');
+    const cardOff = this.querySelector('.card-off');
+
+    cardOn.classList.remove('hidden');
+    cardOff.classList.add('hidden');
+
+    if (previousCard && previousCard !== this) {
+        const previousCardOn = previousCard.querySelector('.card-on');
+        const previousCardOff = previousCard.querySelector('.card-off');
+
+        previousCardOn.classList.add('hidden');
+        previousCardOff.classList.remove('card-on');
+        previousCard.addEventListener('click', flipCard); // Rétablir l'écouteur sur la carte précédente
+    }
+
+    previousCard = this;
+    this.removeEventListener('click', flipCard); // Désactiver le clic sur la carte actuelle
+}
+
+cardsElements.forEach(card => card.addEventListener('click', flipCard));
